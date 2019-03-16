116cc792897858d14630df70cc43e8e069348248
Ahmad <ahmad.khajehnejad@gmail.com>
2019-03-16 03:54:55 +0330

    debug

--- tools.py
+++ tools.py
@@ -7,13 +7,13 @@ def get_train_and_validation_index(ind):
     n_total = ind.size
     if config.classifier.smart_validation:
         index_validation = ind[
-            np.arange(config.classification.validation_portion - 1, n_total, config.classification.validation_portion)]
+            np.arange(config.classifier.validation_portion - 1, n_total, config.classifier.validation_portion)]
         index_tmp = np.ones([n_total])
         index_tmp[index_validation] = 0
         index_train = ind[np.where(index_tmp)[0]]
     else:
         np.random.shuffle(ind)
-        n_train = n_total // config.classification.validation_portion
+        n_train = n_total // config.classifier.validation_portion
         index_train = ind[:n_train]
         index_validation = ind[n_train:]
 

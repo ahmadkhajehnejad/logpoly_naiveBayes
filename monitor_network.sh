240036f55a72bc5d40c02750e3cd0179cc89b0f5
Ahmad <ahmad.khajehnejad@gmail.com>
2020-04-09 04:23:18 +0430

    --

--- monitor_network.sh
+++
@@ -1,8 +0,0 @@
-source activate conpy
-#PASSWORD=$1
-#echo $PASSWORD | sudo -S tcpdump -i lo -n > tcpdump.out &
-tcpdump -i lo -n > tcpdump.out &
-python compute_communication_main.py > comp_comm.out
-sleep 3
-#echo $PASSWORD | sudo -S killall tcpdump
-pkill tcpdump
---
+++ run_distributed.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo starting run_client_nodes
+python run_client_nodes.py > clients.out &
+
+sleep 5
+
+echo starting tcpdump
+tcpdump -i any -n portrange 20000-30000 > tcpdump.out 2>&1 &
+
+echo starting run_center_node
+python run_center_node.py > center.out
+
+echo killing tcpdumpca
+pkill tcpdump
